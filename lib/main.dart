8a4535dcbbdec495011f6dd84997159630b144db
Vedat Biner <vbiner@gmail.com>
2022-04-19 17:55:08 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+// Interaktif deneyim örneği
+// textbox - state'i kendisi tutuyor..
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState() {
+    print("my home page yaratılıyor.");
+  }
+
+  int aktifButon = 0;
+
+  @override
+  void didUpdateWidget(MyHomePage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  bool checkliMi = false;
+  String text = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              onChanged: (value) {
+                print(value);
+                setState((){
+                  text = value;
+                  checkliMi = value.length.isEven;
+                });
+              },
+            ),
+            Text(text),
+            Checkbox(
+                value: checkliMi,
+                onChanged: (value) {
+                  print(value);
+                  setState(() {
+                    if (value != null) {
+                      checkliMi = value;
+                    }
+                  });
+                }),
+            ElevatedButton(
+              // aktif buton balısılabilir halde ise 0, delise null olsun.
+              onPressed: aktifButon == 0 ? (){
+                print("0");
+                setState(() {
+                  aktifButon = (aktifButon + 1) % 3;
+                });
+              } : null,
+              child: Text("0"),
+            ),
+            ElevatedButton(
+              onPressed: aktifButon == 1 ? (){
+                print("1");
+                setState(() {
+                  aktifButon = (aktifButon + 1) % 3;
+                });
+              } : null,
+              child: Text("1"),
+            ),
+            ElevatedButton(
+              onPressed: aktifButon == 2 ? (){
+                print("2");
+                setState(() {
+                  aktifButon = (aktifButon + 1) % 3;
+                });
+              } : null,
+              child: Text("2"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
